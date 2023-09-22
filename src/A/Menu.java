bf3ba3ed19714f16f40203309b860570f3fb699b
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-09-21 21:15:03 -0300

    Comentarios

--- src/A/Menu.java
+++ src/A/Menu.java
@@ -28,6 +28,7 @@ public class Menu extends JFrame {
 	private JMenu mnPeliculas;
 	private JMenuItem mntmAgregar;
 	private JMenuItem mntmListar;
+	private JPanel panel1, panel2;
 	
 	private JPanel contentPane;
 	private static DefaultListModel<Pelicula> modeloMenu;
@@ -61,16 +62,26 @@ public class Menu extends JFrame {
 		mnPeliculas = new JMenu("Peliculas");
 		menuBar.add(mnPeliculas);
 		
+		panel1 = new PanelAgregar();
+		panel2 = new PanelListar();
+
+		// IMPORTANTE. No va a funcionar bien así. Usá en contentPane un layout en null (AbsoluteLayout) y establecé bounds para los panels.
+		// Por qué? Porque ahora están ambos JPanel a la vez, y el BorderLayout no lo permite.
+		contentPane.add(panel1,BorderLayout.CENTER); // Cambiar
+		contentPane.add(panel2,BorderLayout.CENTER); // Cambiar
+		
+
+		// panel2.setDefaultListModel(modeloMenu); No es un método de JPanel, sino de JList.
+		
+		// Por defecto mostramos sólo el panel1. 
+		panel1.setVisible(true);
+		panel2.setVisible(false);
+		
 		mntmAgregar = new JMenuItem("Agregar");
 		mntmAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				contentPane.removeAll();
-				//Abrir panel ingreso peliculas
-				PanelAgregar panel1 = new PanelAgregar();
-				panel1.setDefaultListModel(modeloMenu);
-				contentPane.add(panel1,BorderLayout.CENTER);
-				contentPane.repaint();
-				contentPane.revalidate();
+				// Acá podés alternar la visibilidad de los JPanel con .setVisible(boolean);
+				
 			}
 		});
 		mnPeliculas.add(mntmAgregar);
@@ -78,12 +89,8 @@ public class Menu extends JFrame {
 		mntmListar = new JMenuItem("Listar");
 		mntmListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				contentPane.removeAll();
-				PanelListar panel2 = new PanelListar();
-				panel2.setDefaultListModel(modeloMenu);
-				contentPane.add(panel2,BorderLayout.CENTER);
-				contentPane.repaint();
-				contentPane.revalidate();
+				// Acá podés alternar la visibilidad de los JPanel con .setVisible(boolean);
+				
 			}
 		});
 		mnPeliculas.add(mntmListar);
